d289e71244b925843ef362bfb177fbbb4bba928e
Artur Czepiel <czepiel.artur@gmail.com>
2022-06-10 22:37:32 +0200

    stop. trunacting. files. when there are some errors

--- src/save.py
+++ src/save.py
@@ -45,28 +45,30 @@ for env in Staging(), Production():
     pretalx = Pretalx(client=PretalxClient.from_config(env))
 
     speakers = []
+    subs = pretalx.get_publishable_submissions()
+    extra_speakers_info = pretalx.get_speakers()
+    rooms = [r.name for r in pretalx.get_rooms()]
+
+    sessions = []
+    for s in subs:
+        # Backfill all the data from the other endpoint
+        # To simplify just overwrite full list of objects
+        s.speakers = [extra_speakers_info[s.code] for s in s.speakers]
+        sessions.append(s.dict())
+        speakers += [s1.dict() for s1 in s.speakers]
+
+    schedule = convert_to_schedule(subs, rooms)
+    if "staging" not in env.event_name:
+        # Breaks are hardcoded and work only for production schedule at the
+        # moment.
+        append_breaks(schedule)
+        sort_by_start_time(schedule)
+
+    # Open files at the very end so that in case of API failure we don't
+    # truncate the file.
     with open(env.SESSIONS_PATH, "w") as sessions_fd, open(
         env.SPEAKERS_PATH, "w"
     ) as speakers_fd, open(env.SCHEDULE_PATH, "w") as schedule_fd:
-        subs = pretalx.get_publishable_submissions()
-        extra_speakers_info = pretalx.get_speakers()
-        rooms = [r.name for r in pretalx.get_rooms()]
-
-        sessions = []
-        for s in subs:
-            # Backfill all the data from the other endpoint
-            # To simplify just overwrite full list of objects
-            s.speakers = [extra_speakers_info[s.code] for s in s.speakers]
-            sessions.append(s.dict())
-            speakers += [s1.dict() for s1 in s.speakers]
-
-        schedule = convert_to_schedule(subs, rooms)
-        if "staging" not in env.event_name:
-            # Breaks are hardcoded and work only for production schedule at the
-            # moment.
-            append_breaks(schedule)
-            sort_by_start_time(schedule)
-
         sessions_fd.write(json.dumps(sessions, indent=2, default=serialize))
         speakers_fd.write(json.dumps(speakers, indent=2, default=serialize))
         schedule_fd.write(json.dumps(schedule, indent=2, default=serialize))
